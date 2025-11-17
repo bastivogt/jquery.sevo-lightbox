9f6659a77bd703746aa98d797c7718f8fbc3ea2a
Sevo <sevodo.dessau@gmail.com>
2025-11-17 17:49:58 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+(function($) {
+    console.log("Hello world!");
+    $("#gallery").sevoLightbox({
+        closeContent: "Schließen"
+    });
+}(jQuery));
---
+++ jquery.sevo-lightbox.js
@@ -0,0 +1,83 @@
+(function($) {
+    $.fn.sevoLightbox = function(options) {
+
+        let settings = $.extend({
+            overlayBackgroundColor: "rgba(0, 0, 0, .9)",
+            closeContent: "Close"
+        }, options);
+
+        return this.each(function() {
+            let overlay;
+            let overlayInner;
+            let img;
+            let closeBtn;
+            setOverlayProps();
+
+            $(this).find("a").on("click", function(e) {
+                e.preventDefault();
+                const href = $(this).attr("href");
+
+                img = $("<img />");
+                img.attr("src", href);
+                img.css({
+                    "display": "block",
+                    "max-width": "90%",
+                    "max-height": "90%"
+                });
+
+                overlayInner.append(img);
+                $("html").css("overflow", "hidden");
+                overlay.show();
+            });
+
+            closeBtn.on("click", function(e) {
+                e.stopPropagation();
+                img.remove();
+                overlay.hide();
+                $("html").css("overflow", "auto");
+            });
+
+            function setOverlayProps() {
+                overlay = $("<div></div>");
+                overlay.css({
+                    "background-color": settings.overlayBackgroundColor,
+                    "position": "absolute",
+                    "top": "0px",
+                    "left": "0px",
+                    "bottom": "0px",
+                    "width": "100%",
+                    "height": "100%", 
+                    "display": "none"
+
+
+                });
+
+                overlayInner = $("<div></div>");
+                overlayInner.css({
+                    "width": "100%",
+                    "height": "100%",
+                    "display": "flex",
+                    "justify-content": "center",
+                    "align-items": "center",
+                    "position": "relative"
+                });
+
+                closeBtn = $(`<div>${settings.closeContent}</div>`);
+                closeBtn.css({
+                    "color": "white",
+                    "position": "absolute",
+                    "top": "10px",
+                    "right": "10px",
+                    "cursor": "pointer"
+
+                });
+                overlayInner.append(closeBtn);
+                overlay.append(overlayInner);
+                
+                $("body").append(overlay);
+            }
+
+            
+        });
+    };
+}(jQuery));
